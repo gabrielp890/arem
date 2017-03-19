0d4f2a2f2ed66c4cdee3c14e9a02a50ca8968784
gabriel peña <gabriel pea>
2017-03-19 16:42:01 -0500

    servidor multiples solicitudes

---
+++ test/servidormultiplessolicitudes/ServidorMultiplesSolicitudesTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidormultiplessolicitudes;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class ServidorMultiplesSolicitudesTest {
+    
+    public ServidorMultiplesSolicitudesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class ServidorMultiplesSolicitudes.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        ServidorMultiplesSolicitudes.main(args);
+        ServidorMultiplesSolicitudes sms;
+        
+//        assertEquals("GET /travel HTTP/1.1", sms);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
